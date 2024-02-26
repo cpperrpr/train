80413d2e27caea3461d94fdb54826a7778c2448b
cpperrpr <914672539@qq.com>
2024-02-26 21:07:04 +0800

    实现简单的注册

--- member/src/main/java/com/suiwu/train/member/service/MemberService.java
+++ member/src/main/java/com/suiwu/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.suiwu.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.suiwu.train.member.domain.Member;
+import com.suiwu.train.member.domain.MemberExample;
 import com.suiwu.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -11,5 +16,23 @@ public class MemberService {
     private MemberMapper memberMapper;
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list =  memberMapper.selectByExample(memberExample);
+        if(CollUtil.isNotEmpty(list)){
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+
+        }
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
